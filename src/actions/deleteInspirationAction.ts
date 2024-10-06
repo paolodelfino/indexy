1cca259b1ab1b6b1606e13338a81cfe04ca9df56
paolodelfino <paolodelfino9@gmail.com>
2024-10-06 10:02:57 -1200

    Fix inspiration delete

--- src/actions/deleteInspirationAction.ts
+++ src/actions/deleteInspirationAction.ts
@@ -2,5 +2,5 @@
 import { db } from "@/db/db";
 
 export async function deleteInspirationAction(id: string, prevState: unknown) {
-  await db.deleteFrom("big_paint").where("id", "=", id).execute();
+  await db.deleteFrom("inspiration").where("id", "=", id).execute();
 }
