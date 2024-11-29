f933d7b16a066c341f7205f4d8c818f14341f452
paolodelfino <hackymail12@gmail.com>
2024-11-29 11:31:52 -0800

    Fix bug

--- src/actions/ActionDelete__Inspiration.ts
+++ src/actions/ActionDelete__Inspiration.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { db } from "@/db/db";
+import minioClient from "@/minio/minioClient";
 import schemaInspiration__Delete from "@/schemas/schemaInspiration__Delete";
 import { FormValues } from "@/utils/form";
 
@@ -9,5 +10,17 @@ export default async function ActionDelete__Inspiration(
 ) {
   const validated = schemaInspiration__Delete.parse(values);
 
-  await db.deleteFrom("inspiration").where("id", "=", validated.id).execute();
+  const resources = await db
+    .selectFrom("resource")
+    .where("inspiration_id", "=", validated.id)
+    .select(["sha256", "type"])
+    .execute();
+
+  await Promise.all([
+    db.deleteFrom("inspiration").where("id", "=", validated.id).execute(),
+
+    resources.map(
+      async (it) => await minioClient.removeObject(it.type, it.sha256),
+    ),
+  ]);
 }
